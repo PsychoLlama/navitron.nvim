d4d57ae9b5f4dbc1f0b71a4b86f3da12306cbbcc
Jesse Gibson <JesseTheGibson@gmail.com>
2023-09-24 14:16:37 -0400

    Rewrite initializer in lua

--- lua/navitron.lua
+++ lua/navitron.lua
@@ -1,1 +1,6 @@
-return 'TODO: Create public API'
+return {
+  -- Open the directory with Navitron. Replaces the current window.
+  open = function(directory_path)
+    vim.call('navitron#explore', directory_path)
+  end,
+}
