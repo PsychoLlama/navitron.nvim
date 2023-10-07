5d1b99e135cc4bdc253833f9c77c8d73d927fe68
Jesse Gibson <JesseTheGibson@gmail.com>
2023-10-07 10:03:24 -0400

    Add support for all file types
    This adds support for character devices, block devices, sockets, fifos,
    and everything else.

--- syntax/navitron.lua
+++ syntax/navitron.lua
@@ -2,12 +2,22 @@ local function link(group, target)
   vim.cmd.highlight('default', 'link', group, target)
 end
 
+local function hi(group, ...)
+  vim.cmd.highlight(group, ...)
+end
+
 -- Matches: `{entry_type}:my-file.txt`
-local function create_region(group_name, marker)
+local function create_region(group_name, marker, exclude_hidden_files)
+  local hidden_pattern = ''
+
+  if exclude_hidden_files then
+    hidden_pattern = '[^.]'
+  end
+
   vim.cmd.syntax(
     'region',
     group_name,
-    'start=/^' .. marker .. ':[^.]/',
+    'start=/^' .. marker .. ':' .. hidden_pattern .. '/',
     'end=/$/',
     'contains=navitronEntryType'
   )
@@ -23,9 +33,15 @@ vim.cmd.syntax(
   'contained'
 )
 
-create_region('navitronFileEntry', 'file')
-create_region('navitronDirectoryEntry', 'dir')
+create_region('navitronFileEntry', 'file', true)
+create_region('navitronDirectoryEntry', 'dir', true)
 create_region('navitronSymlinkEntry', 'link')
+create_region('navitronSocketEntry', 'socket')
+create_region('navitronFifoEntry', 'fifo')
+
+create_region('navitronCharacterDeviceEntry', 'cdev')
+create_region('navitronBlockDeviceEntry', 'bdev')
+create_region('navitronUnknownEntry', 'other')
 
 -- Hidden file/directory entry
 vim.cmd.syntax(
@@ -40,3 +56,12 @@ link('navitronDirectoryEntry', 'Directory')
 link('navitronFileEntry', 'Normal')
 link('navitronSymlinkEntry', 'Structure')
 link('navitronHiddenEntry', 'Comment')
+
+-- Unusual, but not too unusual.
+link('navitronSocketEntry', 'Constant')
+link('navitronFifoEntry', 'Constant')
+
+-- Likely to happen if you explore /dev
+link('navitronCharacterDeviceEntry', 'Substitute')
+link('navitronBlockDeviceEntry', 'Substitute')
+link('navitronUnknownEntry', 'Normal')
