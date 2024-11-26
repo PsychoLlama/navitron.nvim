12fcb20917544a4bc7ef512b6c33f14e1faff0c2
Jesse Gibson <JesseTheGibson@gmail.com>
2024-11-26 17:17:30 -0500

    Format local vimrc
    Seems I never ran `stylua` over it.

--- .vimrc.lua
+++ .vimrc.lua
@@ -1,14 +1,17 @@
 local repo = vim.fn.expand('<sfile>:h')
 
 require('core.pkg').on_load(function(plugins)
-  return vim.iter(plugins):map(function(plugin)
-    if plugin.name ~= 'navitron.nvim' then
-      return plugin
-    end
+  return vim
+    .iter(plugins)
+    :map(function(plugin)
+      if plugin.name ~= 'navitron.nvim' then
+        return plugin
+      end
 
-    return vim.tbl_extend('force', plugin, {
-      type = 'path',
-      source = repo,
-    })
-  end):totable()
+      return vim.tbl_extend('force', plugin, {
+        type = 'path',
+        source = repo,
+      })
+    end)
+    :totable()
 end)
