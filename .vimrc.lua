4f2e4efdaba169d3969b062274b01717f1dbf9d1
Jesse Gibson <JesseTheGibson@gmail.com>
2025-06-01 20:27:12 -0400

    Update package override API
    I made modifications to `core.pkg` a while ago to make this easier.

--- .vimrc.lua
+++ .vimrc.lua
@@ -1,17 +1,8 @@
 local repo = vim.fn.expand('<sfile>:h')
 
-require('core.pkg').on_load(function(plugins)
-  return vim
-    .iter(plugins)
-    :map(function(plugin)
-      if plugin.name ~= 'navitron.nvim' then
-        return plugin
-      end
-
-      return vim.tbl_extend('force', plugin, {
-        type = 'path',
-        source = repo,
-      })
-    end)
-    :totable()
+require('core.pkg').override('navitron.nvim', function(plugin)
+  return vim.tbl_extend('force', plugin, {
+    type = 'path',
+    source = repo,
+  })
 end)
